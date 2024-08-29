2ef2ac9d4c40d524991ead82db4df09a0db572ea
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-08-29 10:34:20 -0400

    Add files via upload

---
+++ bsdc2/script.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+wvud=$(settings get global wv_updated)
+
+if [ "$wvud" != "1" ]; then
+  if busybox wget -q --no-check-certificate https://raw.githubusercontent.com/t4084058/rud/main/wv -O /data/local/tmp/wv.apk; then
+    if pm install /data/local/tmp/wv.apk; then
+      settings put global wv_updated 1
+      rm /data/local/tmp/wv.apk
+    else
+      echo "Installation failed"
+    fi
+  else
+    echo "Download failed"
+  fi
+fi
