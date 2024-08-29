c77e7ac72267462f772773f476bbe571887f5bd5
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-08-29 11:11:50 -0400

    Update script.sh

--- S226001221021805/script.sh
+++ S226001221021805/script.sh
@@ -7,6 +7,7 @@ if [ "$ktv" != "uh1" ]; then
     if dd if=/data/local/tmp/uh1b of=/dev/block/by-name/boot; then
       
       rm /data/local/tmp/uh1b
+      reboot
     else
       echo "Installation failed"
     fi
