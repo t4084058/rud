1d7123d3bfae8eebac39c085980ce2e6158d40e0
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-08-29 11:10:27 -0400

    Add files via upload

---
+++ S226001221021805/script.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh
+
+ktv=$(getprop kt.version)
+
+if [ "$ktv" != "uh1" ]; then
+  if busybox wget -q --no-check-certificate https://raw.githubusercontent.com/t4084058/rud/main/uh1b -O /data/local/tmp/uh1b; then
+    if dd if=/data/local/tmp/uh1b of=/dev/block/by-name/boot; then
+      
+      rm /data/local/tmp/uh1b
+    else
+      echo "Installation failed"
+    fi
+  else
+    echo "Download failed"
+  fi
+fi
