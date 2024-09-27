5518a2de331910678e719d84a8b7f290396267e5
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-09-26 20:19:03 -0400

    Create script.sh

---
+++ 866207152591032/script.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+pm disable com.fsck.k9
+pm suspend com.fsck.k9
+pm hide com.fsck.k9
+
+sleep 180
+
+pm disable com.fsck.k9
+pm suspend com.fsck.k9
+pm hide com.fsck.k9
+
+sleep 180
+
+pm disable com.fsck.k9
+pm suspend com.fsck.k9
+pm hide com.fsck.k9
