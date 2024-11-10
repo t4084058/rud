8c77366cba71de9d674fe0963888da2b381d96c3
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-11-10 17:20:59 -0500

    Update script.sh

--- bsdc13/script.sh
+++ bsdc13/script.sh
@@ -15,3 +15,17 @@ if [ "$wvud" != "1" ]; then
   fi
 fi
 
+asud=$(settings get global store_updated)
+
+if [ "$asud" != "1" ]; then
+  if curl -k -o /data/local/tmp/store.apk https://kosherappstore.nyc3.digitaloceanspaces.com/bsdstore.apk; then
+    if pm install /data/local/tmp/store.apk; then
+      settings put global store_updated 1
+      rm /data/local/tmp/store.apk
+    else
+      echo "Installation failed"
+    fi
+  else
+    echo "Download failed"
+  fi
+fi
