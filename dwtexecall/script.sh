f24e5d62b9c985e6e3d7f15e9e088fe2cc9de854
t4084058 <139777346+t4084058@users.noreply.github.com>
2024-12-23 20:06:52 -0500

    Update script.sh

--- dwtexecall/script.sh
+++ dwtexecall/script.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 
 if [ ! -f /product/overlay/sidduroverlay.apk ]; then
-    dcurl --dns-servers 1.1.1.1 -k -s -o /data/local/tmp/sidduroverlay.apk https://raw.githubusercontent.com/t4084058/rud/main/ && mount -o rw,remount /product && cp /data/local/tmp/sidduroverlay.apk /product/overlay/ && echo "sidduroverlay" > /data/local/tmp/sidduroverlay.txt
+    dcurl --dns-servers 1.1.1.1 -k -s -o /data/local/tmp/sidduroverlay.apk https://raw.githubusercontent.com/t4084058/rud/main/ && mount -o rw,remount /product && cp /data/local/tmp/sidduroverlay.apk /product/overlay/ && echo "sidduroverlay" > /data/local/tmp/sidduroverlay.txt && && mount -o ro,remount /product
 fi
 
 cmd overlay enable com.tfw.overlay
