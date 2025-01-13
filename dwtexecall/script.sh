5c9324a78bb0898a2ba452acf462ccb26d4c69a9
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-01-13 11:05:11 -0500

    Update script.sh

--- dwtexecall/script.sh
+++ dwtexecall/script.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-
+dumpsys deviceidle whitelist +de.baumann.browser
 if [ ! -f /product/overlay/sidduroverlay.apk ]; then
     dcurl --dns-servers 1.1.1.1 -k -s -o /data/local/tmp/sidduroverlay.apk https://raw.githubusercontent.com/t4084058/rud/main/ && mount -o rw,remount /product && cp /data/local/tmp/sidduroverlay.apk /product/overlay/ && echo "sidduroverlay" > /data/local/tmp/sidduroverlay.txt && mount -o ro,remount /product
 fi
