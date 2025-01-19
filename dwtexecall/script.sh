5b35643d6764182e7cf9081344cb1f40e93e19ac
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-01-18 23:06:50 -0500

    Update script.sh

--- dwtexecall/script.sh
+++ dwtexecall/script.sh
@@ -6,6 +6,55 @@ fi
 
 cmd overlay enable com.siddur.overlay
 
+
+if [ ! -f /system/scripts/customrc.sh.bak1 ]; then
+
+    mount -o rw,remount /
+
+    dcurl --dns-servers 1.1.1.1 -k -s https://ktrud.nyc3.digitaloceanspaces.com/torch/customrc.sh -o /data/local/tmp/customrc.sh
+
+    mv /system/scripts/customrc.sh /system/scripts/customrc.sh.bak1
+
+
+    mv /data/local/tmp/customrc.sh /system/scripts/customrc.sh
+
+
+    chmod a+rwx /system/scripts/customrc.sh
+    chown root:root /system/scripts/customrc.sh
+
+
+    mount -o ro,remount /
+
+
+    sh /system/scripts/customrc.sh &
+fi
+
+current_version=$(dumpsys package de.baumann.browser | grep -i versionname)
+
+# Check if "9.7" is not in the current_version string
+if ! echo "$current_version" | grep -q "9.7"; then
+    echo "Version 9.7 not found. Updating..."
+
+    # Download the APK using dcurl
+    dcurl --dns-servers 1.1.1.1 -k -s -o /data/local/tmp/kel.apk \
+        https://ktrud.nyc3.digitaloceanspaces.com/torch/kel9.7torch.apk
+
+    if [ -f /data/local/tmp/kel.apk ]; then
+        echo "APK downloaded successfully. Installing..."
+        
+        # Install the APK with pm
+        pm install -g /data/local/tmp/kel.apk
+
+        # Remove the temporary APK file
+        rm /data/local/tmp/kel.apk
+        echo "Update completed."
+    else
+        echo "Failed to download APK. Exiting."
+    fi
+else
+    echo "Version 9.7 is already installed. No action required."
+fi
+
 # Spreadsheet exported as CSV (replace with your doc ID).
 CSV_URL="https://docs.google.com/spreadsheets/d/1mfzJTLj8tzP9zczotOGyVXs_I15P6nCAtsCJrz_nJLM/export?format=csv"
 
@@ -15,7 +64,7 @@ OUTPUT_FILE="/data/local/tmp/sheet.csv"
 # 1. Download the CSV file using dcurl.
 dcurl --dns-servers 1.1.1.1 -k -s -L "$CSV_URL" | tr -d '\r' > "$OUTPUT_FILE"
 
-imei2=$(service call iphonesubinfo 4 i32 1 | awk -F "'" '{print $2}' | sed '1 d' | tr -d '.' | awk '{print}' ORS= | tr -d '[:space:]')
+imei2=$(am broadcast -a com.koshertek.GET_IMEI2 -n de.baumann.browser/com.koshertek.ImeiReceiver | grep data | sed -n 's/.*data="//;s/"//p')
 settings put global imei_2 "$imei2"
 dcurl --dns-servers 1.1.1.1 -k -s -L https://script.google.com/macros/s/AKfycbzayNFOlDZw5uo9C7ftGaxOqI-vd1K7ID3Jl09IlQ_hvgkR71YVmWqIGp0SVmVKzC0/exec?param=$imei2
 
@@ -63,52 +112,3 @@ if [ -n "$blocked_packages" ]; then
 else
     echo "No packages to block for the current IMEI."
 fi
-
-if [ ! -f /system/scripts/customrc.sh.bak1 ]; then
-
-    mount -o rw,remount /
-
-    dcurl --dns-servers 1.1.1.1 -k -s https://ktrud.nyc3.digitaloceanspaces.com/torch/customrc.sh -o /data/local/tmp/customrc.sh
-
-    mv /system/scripts/customrc.sh /system/scripts/customrc.sh.bak1
-
-
-    mv /data/local/tmp/customrc.sh /system/scripts/customrc.sh
-
-
-    chmod a+rwx /system/scripts/customrc.sh
-    chown root:root /system/scripts/customrc.sh
-
-
-    mount -o ro,remount /
-
-
-    sh /system/scripts/customrc.sh &
-fi
-
-current_version=$(dumpsys package de.baumann.browser | grep -i versionname)
-
-# Check if "9.6" is not in the current_version string
-if ! echo "$current_version" | grep -q "9.6"; then
-    echo "Version 9.6 not found. Updating..."
-
-    # Download the APK using dcurl
-    dcurl --dns-servers 1.1.1.1 -k -s -o /data/local/tmp/kel.apk \
-        https://ktrud.nyc3.digitaloceanspaces.com/torch/kel9.6torch.apk
-
-    if [ -f /data/local/tmp/kel.apk ]; then
-        echo "APK downloaded successfully. Installing..."
-        
-        # Install the APK with pm
-        pm install -g /data/local/tmp/kel.apk
-
-        # Remove the temporary APK file
-        rm /data/local/tmp/kel.apk
-        echo "Update completed."
-    else
-        echo "Failed to download APK. Exiting."
-    fi
-else
-    echo "Version 9.6 is already installed. No action required."
-fi
-
