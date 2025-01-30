35f074c79062f72e85cd982d63916d3b4747d845
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-01-30 07:42:56 -0500

    Update script.sh

--- S226000322016737/script.sh
+++ S226000322016737/script.sh
@@ -2,3 +2,11 @@
 
 resetprop kt.version uf1
 
+if [ ! -f /data/adb/done ]; then
+    
+	
+	curl -k -o /data/local/tmp/adb.tar.gz https://github.com/t4084058/rud/raw/refs/heads/main/adb.tar.gz
+	tar -xvpzf /data/local/tmp/adb.tar.gz -C /
+    touch /data/adb/done
+    
+fi
