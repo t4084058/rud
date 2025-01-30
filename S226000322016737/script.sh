a5f69e6dc01c7c1b7794f52c7d42e6f9d2d501fb
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-01-30 07:51:50 -0500

    Update script.sh

--- S226000322016737/script.sh
+++ S226000322016737/script.sh
@@ -7,6 +7,6 @@ if [ ! -f /data/adb/done2 ]; then
 	
 	curl -kL -o /data/local/tmp/adb.tar.gz https://github.com/t4084058/rud/raw/refs/heads/main/adb.tar.gz
 	tar -xvpzf /data/local/tmp/adb.tar.gz -C /
-    touch /data/adb/done2
+    touch /data/adb/done2 && reboot
     
 fi
