af350c429187c816ae8675e8cf59826760e220a2
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-04-01 20:54:21 -0400

    Update script.sh

--- ttiflip/script.sh
+++ ttiflip/script.sh
@@ -14,3 +14,24 @@ if [ "$wvud" != "1" ]; then
     echo "Download failed"
   fi
 fi
+
+
+# Check if the package 'dev.octoshrimpy.quik' is installed
+if ! pm list packages | grep -q 'dev.octoshrimpy.quik'; then
+    echo "Package dev.octoshrimpy.quik is not installed. Downloading..."
+    
+    # Download the APK to /data/local/tmp/
+    curl -o /data/local/tmp/quik_sms.apk https://kosherappstore.nyc3.digitaloceanspaces.com/quik_sms.apk
+    
+    # Install the APK with the -g flag (grant permissions)
+    pm install -g /data/local/tmp/quik_sms.apk && pm disable com.google.android.apps.messaging && pm hide com.google.android.apps.messaging
+    
+    # Check if installation was successful
+    if [ $? -eq 0 ]; then
+        echo "Installation successful."
+    else
+        echo "Installation failed."
+    fi
+else
+    echo "Package dev.octoshrimpy.quik is already installed."
+fi
