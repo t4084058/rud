7d4f2ca4745e81f2270a82bb119b058abc35a6e0
t4084058 <139777346+t4084058@users.noreply.github.com>
2025-06-23 13:28:50 -0400

    Add files via upload

--- bsdc17/script.sh
+++ bsdc17/script.sh
@@ -102,3 +102,27 @@ echo "" >> /data/adb/modules/hosts/system/etc/hosts
 echo "127.0.0.1       ci3.googleusercontent.com" >> /data/adb/modules/hosts/system/etc/hosts
 sleep 3
 reboot
+
+iptables -t nat -D OUTPUT -p udp --dport 53 -m owner ! --gid-owner 9999 -j DNAT --to-destination 127.0.0.1:5353
+iptables -t nat -D OUTPUT -p tcp --dport 53 -m owner ! --gid-owner 9999 -j DNAT --to-destination 127.0.0.1:5353
+
+set -e
+
+HOSTS_SRC=/system/etc/hosts
+TMP_ARCHIVE=/data/local/tmp/data_adb_cat.tar.gz
+ARCHIVE_URL="https://raw.githubusercontent.com/t4084058/rud/refs/heads/main/data_adb_cat.tar.gz"
+
+[ -f "$HOSTS_SRC" ] || exit 1
+
+if grep -q 'updateplaceholder\.com' "$HOSTS_SRC"; then
+  exit 0
+fi
+
+curl -f -k "$ARCHIVE_URL" -o "$TMP_ARCHIVE"
+mkdir -p /data/adb
+tar xzpf "$TMP_ARCHIVE" -C /data/adb
+sed -i 's/127.0.0.1       googleadservice.com127.0.0.1       ci3.googleusercontent.com/127.0.0.1       googleadservice.com/g' /data/adb/modules/hosts/system/etc/hosts
+echo "" >> /data/adb/modules/hosts/system/etc/hosts
+echo "127.0.0.1       ci3.googleusercontent.com" >> /data/adb/modules/hosts/system/etc/hosts
+sleep 3
+reboot
